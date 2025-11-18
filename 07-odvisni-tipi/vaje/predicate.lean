c68d6fd1863b779770b5b53c1d41884406c6a792
Zala Plesec <zp25130@student.uni-lj.si>
2025-11-18 12:57:16 +0000

    x

--- 07-odvisni-tipi/vaje/predicate.lean
+++ 07-odvisni-tipi/vaje/predicate.lean
@@ -5,13 +5,34 @@ variable (r : Prop)
 -- Izjave napišite na list papirja, nato pa jih dokažite v datoteki.
 
 theorem eq1 : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) :=
-  sorry
+  by
+    apply Iff.intro
+    · intro notExistsPx x px
+      apply notExistsPx
+      exact ⟨ x, px⟩
+    · intro forallNotPx existsPx
+      obtain ⟨x, px⟩ := existsPx
+      have notPx := forallNotPx
+      apply notPx
+      exact px
+
 
 theorem eq2 : (r → ∀ x, p x) ↔ (∀ x, r → p x) :=
-  sorry
+  by
+    apply Iff.intro
+    · intro h1 x hr
+      have allPx := h1 hr
+      exact allPx x
+    · intro h2 hr x
+      have rtoallPx := h2 x
+      have allPx := rtoallPx hr
+      exact allPx
 
-theorem eq3 : r ∧ (∃ x, p x) ↔ (∃ x, r ∧ p x) := by
-  sorry
+theorem eq3 : r ∧ (∃ x, p x) ↔ (∃ x, r ∧ p x) :=
+  by
+    apply Iff.intro
+    · apply And.intro
+
 
 theorem eq4 : r ∨ (∀ x, p x) → (∀ x, r ∨ p x) :=
   sorry
@@ -23,7 +44,32 @@ open Classical
 #check Classical.em
 
 theorem eq5 : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) :=
- sorry
+  by
+    apply Iff.intro
+    · intro notForallPx
+      apply Classical.byContradiction
+      intro notExistsnotPx
+      apply notForallPx
+      intro x
+
+      apply Classical.byContradiction
+      intro notPx
+      apply notExistsnotPx
+      exact ⟨x, notPx⟩
+
+    · intro h1 h2
+      obtain ⟨ x, NotPx ⟩ := h1
+      have h3 := h2 x
+      apply NotPx
+      exact h3
+
 
 theorem eq6 : r ∨ (∀ x, p x) ↔ (∀ x, r ∨ p x) :=
-  sorry
+  by
+    apply Iff.intro
+    · intro h1
+      sorry
+    · intro h1
+      have h2 := Classical.em r
+      sorry cases h2 with
+      | inl r1 => sorry
